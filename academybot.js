8c1a08b3c11f71a05c514edd22aecd75c234f15b
Emi Markovska <markovska.emi@gmail.com>
2016-09-29 15:24:09 +0200

    integration for thomas

--- academybot.js
+++ academybot.js
@@ -6,13 +6,13 @@
 var Bot = require('slackbots');
 
 var settings = {
-  token:  'TOKEN', //Replace this with your token
-  name: 'AcademyBot', //Replace this with the Bot's name
-  id: 'academybot' //Replace this with the Bot's handle
+  token:  'xoxb-85548971696-QskZVIVoXyVo4ojKxd1q2GOq', //Replace this with your token
+  name: 'Tomas Shelby', //Replace this with the Bot's name
+  id: 'thomas' //Replace this with the Bot's handle
 };
 
 var params = {
-  icon_emoji: ':unicorn_face:', //replace this with whatever icon you want the bot to post as
+//  icon_emoji: ':unicorn_face:', //replace this with whatever icon you want the bot to post as
   link_names: 1 //Ensures formatting in messages posted by your bot is respected
 };
 
